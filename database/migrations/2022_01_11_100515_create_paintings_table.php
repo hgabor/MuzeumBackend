c4eaba6b4c54db86f0a18a6d1672ef9b9282805f
Petrik Tanar <tanar@petrik.hu>
2022-01-11 11:17:30 +0100

    Initial commit

---
+++ database/migrations/2022_01_11_100515_create_paintings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaintingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('paintings', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->integer('year');
+            $table->boolean('on_display');
+            $table->timestamps();
+            $table->foreignIdFor(User::class);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('paintings');
+    }
+}
