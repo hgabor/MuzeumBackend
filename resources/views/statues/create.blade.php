750778083d2179390377a3b3768a9275c01d60d9
Petrik Tanar <tanar@petrik.hu>
2022-01-28 15:09:47 +0100

    Added CSS, laravel mix

--- resources/views/statues/create.blade.php
+++ resources/views/statues/create.blade.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ mix('css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <link rel="stylesheet" href="{{ mix('css/masik.css') }}">
     <title>Document</title>
 </head>
 <body>
@@ -41,7 +44,7 @@
             @enderror
         </div>
         <div>
-            <input type="submit" value="Create">
+            <input type="submit" value="Create" class="btn btn-primary">
         </div>
     </form>
 </body>
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,7 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+    .css('node_modules/bootstrap/dist/css/bootstrap.css', 'public/css')
+    .css('resources/css/app.css', 'public/css')
+    .css('resources/css/masik.css', 'public/css')
+    .sourceMaps(true);
